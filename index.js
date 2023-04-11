2b273b0b9112e121234cea1ce246fd39f279c54e
alu- <alu@byteberry.net>
2023-04-11 19:16:53 +0200

    Try using certbot tls

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
+const https = require("https");
+const fs = require("fs");
+const options = {
+    key: fs.readFileSync("/etc/letsencrypt/live/planning-poker.io/privkey.pem", 'utf8'),
+    cert: fs.readFileSync("/etc/letsencrypt/live/planning-poker.io/cert.pem", 'utf8'),
+    ca: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8')
+};
+const httpServer = https.createServer(options, app);
+const httpsServer = https.createServer(options, app);
 const {Server} = require("socket.io");
-const io = new Server(server);
+const io = new Server(httpsServer);
 const path = require('node:path');
 const socketEndpoints = require("./src/backend/Socket/SocketHandler");
-const port = process.env.PORT || 80;
+const httpPort = process.env.PORT || 80;
+const https_port = process.env.HTTPS_PORT || 443;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -20,6 +29,9 @@ const onConnection = (socket) => {
 }
 io.on('connection', onConnection);
 
-server.listen(port, () => {
-    console.log('Listening on *:' + port);
+httpServer.listen(httpPort, "0.0.0.0", () => {
+    console.log('Listening on *:' + httpPort);
+});
+httpsServer.listen(https_port, "0.0.0.0", () => {
+    console.log('Listening on *:' + https_port);
 });
